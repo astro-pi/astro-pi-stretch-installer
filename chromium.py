f8347f4ed08fed8a26f34ec45b035728a1588c1c
Ben Nuttall <ben@bennuttall.com>
2018-10-12 14:32:29 +0100

    Check Chromium settings are present first

--- chromium.py
+++ chromium.py
@@ -1,14 +1,19 @@
 import json
 
 prefs_file = '/home/pi/.config/chromium/Default/Preferences'
+prefs = {}
 
-with open(prefs_file, 'r') as f:
-    prefs = json.load(f)
+try:
+    with open(prefs_file, 'r') as f:
+        prefs = json.load(f)
+except FileNotFoundError:
+    print("Chromium settings not found - please launch Chromium once before running this script")
 
-prefs['session'] = {
-    'restore_on_startup': 4,
-    'startup_urls': ['https://astro-pi.org/', 'http://www.raspberrypi.org/']
-}
+if prefs:
+    prefs['session'] = {
+        'restore_on_startup': 4,
+        'startup_urls': ['http://rpf.io/ap-msl-guide']
+    }
 
-with open(prefs_file, 'w') as f:
-    json.dump(prefs, f)
+    with open(prefs_file, 'w') as f:
+        json.dump(prefs, f)
