e95386af21a03aebe0bb8b3f342aec207cd3b409
Ben Nuttall <ben@bennuttall.com>
2018-10-12 16:46:47 +0100

    Move apt and pip dependencies to files

--- setup.sh
+++ setup.sh
@@ -4,18 +4,14 @@ set -eu
 
 source /etc/os-release
 
+t=`date '+%H:%M:%S'`
+
 # Check we're on Raspbian Stretch (Debian 9)
 
 if [ $VERSION_ID -eq 9 ]; then
-    echo -ne "\nYou are running Raspbian Stretch. Starting Astro Pi setup"
-    sleep 0.2
-    echo -n "."
-    sleep 0.2
-    echo -n "."
-    sleep 0.2
-    echo -n "."
+    echo "$t You are running Raspbian Stretch. Starting Astro Pi setup..."
 else
-    echo -e "\nYou seem to be using {$PRETTY_NAME}. This installer is for Raspbian Stretch. Please download Raspbian Stretch from the Raspberry Pi website http://rpf.io/raspbian"
+    echo "You seem to be using {$PRETTY_NAME}. This installer is for Raspbian Stretch. Please download Raspbian Stretch from the Raspberry Pi website http://rpf.io/raspbian"
     exit 1
 fi
 
@@ -24,10 +20,10 @@ fi
 chromium=`dpkg -l | grep chromium | wc -l`
 if [ $chromium -gt 0 ]; then
     desktop=true
-    echo -ne "\n\nIt looks like you are running Raspbian Desktop.\n"
+    echo "It looks like you are running Raspbian Desktop"
 else
     desktop=false
-    echo -ne "\n\nIt looks like you are running Raspbian Lite.\n"
+    echo -e "It looks like you are running Raspbian Lite"
 fi
 
 # Check if git was already installed
@@ -41,23 +37,26 @@ fi
 
 # Update and install apt packages
 
-echo -ne "\nUpdating and upgrading your apt packages"
+echo "Updating and upgrading your apt packages"
+t=`date '+%H:%M:%S'`
+echo "$t Running update"
+sudo apt-get -qq update > /dev/null
+t=`date '+%H:%M:%S'`
+echo "$t Running upgrade"
+sudo apt-get -qqy upgrade > /dev/null
+t=`date '+%H:%M:%S'`
+echo "$t Running dist-upgrade"
+sudo apt-get -qqy dist-upgrade > /dev/null
 
-sudo apt-get -qq update
-echo -n "."
-sudo apt-get -qqy upgrade
-echo -n "."
-sudo apt-get -qqy dist-upgrade
-echo -n "."
-sudo apt-get install \
-    zip wget tree git \
-    sense-hat i2c-tools libatlas3-base \
-    python3 python3-pip python3-gpiozero python3-rpi.gpio python3-pygame \
-    python3-picamera python3-colorzero python3-gdal \
-    libhdf5-100 libharfbuzz0b libwebp6 libtiff5 libjasper1 libilmbase12 \
-    libopenexr22 libgstreamer1.0-0 libavcodec57 libavformat57 libavutil55 \
-    libswscale4 libgtk-3-0 libpangocairo-1.0-0 libpango-1.0-0 libatk1.0-0 \
-    libcairo-gobject2 libcairo2 libgdk-pixbuf2.0-0 -qqy > /dev/null
+mapfile -t packages < packages.txt
+
+t=`date '+%H:%M:%S'`
+echo "$t Installing new apt packages..."
+for package in "${packages[@]}"; do
+    t=`date '+%H:%M:%S'`
+    echo "$t Installing $package..."
+    sudo apt-get install $package -qqy > /dev/null
+done
 
 # Clone this repo to have access to test files and desktop backgrounds
 
@@ -73,43 +72,46 @@ fi
 
 # Install Python packages from PyPI/piwheels - versions specified in requirements.txt
 
-echo -e "\n\nUpdating and upgrading your Python packages..."
+mapfile -t py_packages < requirements.txt
+echo "$t Updating and upgrading your Python packages..."
 
-sudo pip3 install -qr requirements.txt
+for package in "${py_packages[@]}"; do
+    t=`date '+%H:%M:%S'`
+    echo "$t Installing $package..."
+    sudo pip3 install -q $package
+done
 
-echo -e "\nTesting importing your Python packages..."
+echo "Testing importing your Python packages..."
 
 if python3 -W ignore test.py; then
-    echo -e "\nAll Python libraries imported ok\n"
+    echo "All Python libraries imported ok"
 else
-    echo -e "\nThere were errors with the Python libraries. See above for more information.\n"
+    echo "There were errors with the Python libraries. See above for more information."
 fi
 
 # Set Chromium homepage and bookmarks
 
 if $desktop; then
-    echo -ne "Setting your Chromium homepage and bookmarks...\n"
+    echo "Setting your Chromium homepage and bookmarks..."
     python3 chromium.py
 fi
 
 # Download some desktop background images
 
 if $desktop; then
-    echo -ne "\nInstalling desktop backgrounds\n"
+    echo "$Installing desktop backgrounds"
     sudo cp desktop-backgrounds/* /usr/share/rpd-wallpaper/
-
     # Set the desktop background to MSL
-
     sed -i -e 's/road.jpg/mission-space-lab.jpg/g' /home/pi/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
-
-    echo -e "Astro Pi Installation complete! Restarting desktop session in 5 seconds...\n"
-
-    sleep 5
-
-    sudo systemctl restart lightdm
 else
-    echo -e "Astro Pi Installation complete!\n"
+    echo "Astro Pi Installation complete!"
 fi
 
 cd ../
 rm -rf astro-pi-stretch-installer
+
+if $desktop; then
+    echo "Astro Pi Installation complete! Restarting desktop session in 5 seconds..."
+    sleep 5
+    sudo systemctl restart lightdm
+fi
