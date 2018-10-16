e6d8f410ba810e728aecda576f8fa4c0f1650786
topshed <top_shed@yahoo.com>
2018-10-16 15:37:29 +0100

    more conditions

--- chromium.py
+++ chromium.py
@@ -5,17 +5,24 @@ master_prefs_file = '/usr/lib/chromium-browser/master_preferences'
 prefs = {}
 
 try:
-    with open(prefs_file, 'a') as f:
+    with open(prefs_file, 'r') as f:
+        location = "local"
         prefs = json.load(f)
 except FileNotFoundError:
     #print("Chromium settings not found - please launch Chromium once before running this script")
     print("User Chromium settings not found - Modifying master settings")
-    with open(master_prefs_file, 'a') as f:
+    with open(master_prefs_file, 'r') as f:
         prefs = json.load(f)
+        location = "master"
 
 if prefs:
     prefs['session'] = {
         'restore_on_startup': 4,
         'startup_urls': ['http://rpf.io/ap-msl-guide']
     }
-json.dump(prefs,f)
+if location == "master":
+    with open(master_prefs_file, 'r') as f:
+        json.dump(prefs,f)
+elif loaction == "local":
+    with open(prefs_file, 'r') as f:
+        json.dump(prefs,f)
